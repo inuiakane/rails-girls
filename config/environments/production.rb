8db39e212dd6fc74211d08c0d2e8ab58192f50e5
inuiakane <akane.inui@avasys.jp>
2015-05-19 06:32:31 +0000

    edit config2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,9 +60,7 @@ Rails.application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
-  #20150519
-  config.serve_static_assets = true
-  config.assets.compile = true
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
